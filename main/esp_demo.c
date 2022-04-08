75260a746eb98a345a8eabf68d5ca61c25342539
Dmitri Lyalikov <Dlyalikov01@manhattan.edu>
2022-04-08 12:42:44 -0400

    initial wifi config/handler

--- main/esp_demo.c
+++ main/esp_demo.c
@@ -33,9 +33,14 @@
 
 #include "driver/gpio.h"
 #include "driver/adc.h"
-#include "esp_adc_cal.h"
 #include "nvs_flash.h"
 #include "L298N.h"
+
+#include "esp_adc_cal.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include <tcpip_adapter.h>
 
 #define DEFAULT_VREF 1500        // ADCout = (Vin, ADC*2^12)/Vref
 #define NO_OF_SAMPLES 1
@@ -96,6 +101,18 @@ void vUpdateQueue(float ulNewValue)
 static void vReadQueue(xSense_t *pxData)
 {
     xQueuePeek(xSense_Queue, pxData, portMAX_DELAY);
+}
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    if (event->event_id == SYSTEM_EVENT_GOT_IP){
+        printf("Got IP Address: " IPSTR "\n",
+        iP2STR(&event->event_info.got_ip.ip_info.ip));
+    }
+    if (event->event_id == SYSTEM_EVENT_STA_START) {
+        ESP_ERROR_CHECK(esp_wifi_connect());
+    }
+    return ESP_OK;
 }
 
 /**
@@ -199,6 +216,22 @@ static void vPIDCompute(void *pvParameter)
 void app_main(void)
 {   
     printf("********ESP32 7x24 Application********\n");
+    nvs_flash_init();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    wifi_config_t sta_config = {
+        .sta = {
+            .ssid = "admin",
+            .password = "password"
+            .bssid_set = 0
+        }
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    
     printf("***Temperature Set to %f Celsius***\n", set_temp);
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(ADC_init());
