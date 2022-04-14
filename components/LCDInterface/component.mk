c4f82ba4887efff1fa915207031de31e7cf94f83
Dmitri Lyalikov <Dlyalikov01@manhattan.edu>
2022-04-14 13:30:16 -0400

    Initial rev LCD Driver

---
+++ components/LCDInterface/component.mk
@@ -0,0 +1,2 @@
+# By default, include only the include/ dir.
+COMPONENT_ADD_INCLUDEDIRS = include
--- main/esp_demo.c
+++ main/esp_demo.c
@@ -47,8 +47,7 @@
 #include "esp_wpa2.h"
 #include "esp_netif.h"
 
-#include "smbus.h"
-#include "i2c-lcd1602.h"
+#include "LCDInterface.h"
 
 // #include "certs.h"
 //#include "aws_iot_config.h"
@@ -77,30 +76,6 @@
 #define MAX_INTEGRAL 10.0        // Max Integral Value
 #define EPSILON      0.01
 
-#define I2C_MASTER_NUM           I2C_NUM_0
-#define I2C_MASTER_TX_BUF_LEN    0                     // disabled
-#define I2C_MASTER_RX_BUF_LEN    0                     // disabled
-#define I2C_MASTER_FREQ_HZ       100000
-#define I2C_MASTER_SDA_IO        CONFIG_I2C_MASTER_SDA
-#define I2C_MASTER_SCL_IO        CONFIG_I2C_MASTER_SCL
-
-static void i2c_master_init(void)
-{
-    int i2c_master_port = I2C_MASTER_NUM;
-    i2c_config_t conf;
-    conf.mode = I2C_MODE_MASTER;
-    conf.sda_io_num = I2C_MASTER_SDA_IO;
-    conf.sda_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
-    conf.scl_io_num = I2C_MASTER_SCL_IO;
-    conf.scl_pullup_en = GPIO_PULLUP_DISABLE;  // GY-2561 provides 10kΩ pullups
-    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
-    conf.clk_flags = 0;
-    i2c_param_config(i2c_master_port, &conf);
-    i2c_driver_install(i2c_master_port, conf.mode,
-                       I2C_MASTER_RX_BUF_LEN,
-                       I2C_MASTER_TX_BUF_LEN, 0);
-}
-
 static const float kp = 2;       //Integral Ratio
 static const float ki = 5;       // Differential ratio 1
 static const float kd = 1;      // Sum of errors
@@ -120,6 +95,9 @@ static SemaphoreHandle_t xQueueMutex;
 static EventGroupHandle_t s_wifi_event_group;
 static esp_netif_t *sta_netif = NULL;
 static int s_retry_num = 0;
+
+uint8_t LCD_pins[6] = {GPIO_NUM_23, GPIO_NUM_19, GPIO_NUM_18, GPIO_NUM_17, GPIO_NUM_16, GPIO_NUM_15};
+
 
 typedef struct xSense_t
 {
@@ -521,218 +499,6 @@ static void vPIDCompute(void *pvParameter)
     }
 }
 
-void lcd1602_task(void * pvParameter)
-{
-    // Set up I2C
-    i2c_master_init();
-    i2c_port_t i2c_num = I2C_MASTER_NUM;
-    uint8_t address = CONFIG_LCD1602_I2C_ADDRESS;
-
-    // Set up the SMBus
-    smbus_info_t * smbus_info = smbus_malloc();
-    ESP_ERROR_CHECK(smbus_init(smbus_info, i2c_num, address));
-    ESP_ERROR_CHECK(smbus_set_timeout(smbus_info, 1000 / portTICK_RATE_MS));
-
-    // Set up the LCD1602 device with backlight off
-    i2c_lcd1602_info_t * lcd_info = i2c_lcd1602_malloc();
-    ESP_ERROR_CHECK(i2c_lcd1602_init(lcd_info, smbus_info, true,
-                                     LCD_NUM_ROWS, LCD_NUM_COLUMNS, LCD_NUM_VISIBLE_COLUMNS));
-
-    ESP_ERROR_CHECK(i2c_lcd1602_reset(lcd_info));
-
-    // turn off backlight
-    //_wait_for_user();
-    i2c_lcd1602_set_backlight(lcd_info, false);
-
-    // turn on backlight
-    //_wait_for_user();
-    i2c_lcd1602_set_backlight(lcd_info, true);
-
-    //_wait_for_user();
-    i2c_lcd1602_set_cursor(lcd_info, true);
-
-    //_wait_for_user();
-    i2c_lcd1602_move_cursor(lcd_info, 0, 0);
-    i2c_lcd1602_write_char(lcd_info, 'A');
-
-    //_wait_for_user();
-    i2c_lcd1602_move_cursor(lcd_info, 8, 0);
-    i2c_lcd1602_write_char(lcd_info, 'B');
-
-    //_wait_for_user();
-    i2c_lcd1602_move_cursor(lcd_info, 15, 1);
-    i2c_lcd1602_write_char(lcd_info, 'C');
-
-    //  _wait_for_user();
-    i2c_lcd1602_move_cursor(lcd_info, 0, 1);
-    i2c_lcd1602_set_blink(lcd_info, true);
-
-    // _wait_for_user();
-    i2c_lcd1602_write_char(lcd_info, 'D');
-    i2c_lcd1602_set_right_to_left(lcd_info);
-    i2c_lcd1602_write_char(lcd_info, 'E');
-    i2c_lcd1602_set_left_to_right(lcd_info);
-
-    // _wait_for_user();
-    i2c_lcd1602_set_display(lcd_info, false);
-
-    // _wait_for_user();
-    i2c_lcd1602_move_cursor(lcd_info, 7, 1);
-    i2c_lcd1602_write_char(lcd_info, 'F');
-
-    // _wait_for_user();
-    i2c_lcd1602_set_display(lcd_info, true);
-
-    // _wait_for_user();
-    i2c_lcd1602_set_blink(lcd_info, false);  // cursor should still be on
-
-     // _wait_for_user();
-    i2c_lcd1602_set_cursor(lcd_info, false);
-
-    // _wait_for_user();
-    i2c_lcd1602_home(lcd_info);
-    i2c_lcd1602_write_string(lcd_info, "abcdefghijklmnopqrstuvwxyz0123456789.,-+ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-
-    // _wait_for_user();
-    for (int i = 0; i < 8; ++i)
-    {
-        i2c_lcd1602_scroll_display_left(lcd_info);
-        vTaskDelay(200 / portTICK_RATE_MS);
-    }
-
-   // _wait_for_user();
-    for (int i = 0; i < 8; ++i)
-    {
-        i2c_lcd1602_scroll_display_right(lcd_info);
-    }
-
-   //  _wait_for_user();
-    i2c_lcd1602_move_cursor(lcd_info, 8, 0);
-    i2c_lcd1602_set_cursor(lcd_info, true);
-
-    //_wait_for_user();
-    for (int i = 0; i < 5; ++i)
-    {
-        i2c_lcd1602_move_cursor_right(lcd_info);
-    }
-
-    //_wait_for_user();
-    for (int i = 0; i < 3; ++i)
-    {
-        i2c_lcd1602_move_cursor_left(lcd_info);
-    }
-
-    // _wait_for_user();
-    i2c_lcd1602_set_auto_scroll(lcd_info, true);
-    for (int i = 0; i < 5; ++i)
-    {
-        i2c_lcd1602_write_char(lcd_info, '>');
-        vTaskDelay(200 / portTICK_RATE_MS);
-    }
-
-
-   //  _wait_for_user();
-    i2c_lcd1602_set_right_to_left(lcd_info);
-    for (int i = 0; i < 5; ++i)
-    {
-        i2c_lcd1602_write_char(lcd_info, '<');
-        vTaskDelay(200 / portTICK_RATE_MS);
-    }
-
-    // _wait_for_user();
-    i2c_lcd1602_set_auto_scroll(lcd_info, false);
-    for (int i = 0; i < 5; ++i)
-    {
-        i2c_lcd1602_write_char(lcd_info, '+');
-        vTaskDelay(200 / portTICK_RATE_MS);
-    }
-
-   //  _wait_for_user();
-    i2c_lcd1602_set_left_to_right(lcd_info);
-    for (int i = 0; i < 5; ++i)
-    {
-        i2c_lcd1602_write_char(lcd_info, '>');
-        vTaskDelay(200 / portTICK_RATE_MS);
-    }
-
-   // _wait_for_user();
-    i2c_lcd1602_clear(lcd_info);
-    i2c_lcd1602_set_cursor(lcd_info, false);
-
-   // _wait_for_user();
-    // https://github.com/agnunez/ESP8266-I2C-LCD1602/blob/master/examples/CustomChars/CustomChars.ino
-    uint8_t bell[8]  = {0x4, 0xe, 0xe, 0xe, 0x1f, 0x0, 0x4};
-    uint8_t note[8]  = {0x2, 0x3, 0x2, 0xe, 0x1e, 0xc, 0x0};
-    uint8_t clock[8] = {0x0, 0xe, 0x15, 0x17, 0x11, 0xe, 0x0};
-    uint8_t heart[8] = {0x0, 0xa, 0x1f, 0x1f, 0xe, 0x4, 0x0};
-    uint8_t duck[8]  = {0x0, 0xc, 0x1d, 0xf, 0xf, 0x6, 0x0};
-    uint8_t check[8] = {0x0, 0x1 ,0x3, 0x16, 0x1c, 0x8, 0x0};
-    uint8_t cross[8] = {0x0, 0x1b, 0xe, 0x4, 0xe, 0x1b, 0x0};
-    uint8_t retarrow[8] = { 0x1, 0x1, 0x5, 0x9, 0x1f, 0x8, 0x4};
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_0, bell);
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_1, note);
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_2, clock);
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_3, heart);
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_4, duck);
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_5, check);
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_6, cross);
-    i2c_lcd1602_define_char(lcd_info, I2C_LCD1602_INDEX_CUSTOM_7, retarrow);
-
-    // after defining custom characters, DDRAM address must be set by home() or moving the cursor
-    i2c_lcd1602_move_cursor(lcd_info, 0, 0);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_0);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_1);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_2);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_3);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_4);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_5);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_6);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_CUSTOM_7);
-
-   // _wait_for_user();
-    i2c_lcd1602_move_cursor(lcd_info, 0, 1);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_ALPHA);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_BETA);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_THETA);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_PI);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_OMEGA);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_SIGMA);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_INFINITY);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_DEGREE);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_ARROW_LEFT);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_ARROW_RIGHT);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_SQUARE);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_DOT);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_DIVIDE);
-    i2c_lcd1602_write_char(lcd_info, I2C_LCD1602_CHARACTER_BLOCK);
-
-   // _wait_for_user();
-    i2c_lcd1602_clear(lcd_info);
-    i2c_lcd1602_set_cursor(lcd_info, true);
-    uint8_t c = 0;
-    uint8_t col = 0;
-    uint8_t row = 0;
-    while (1)
-    {
-        i2c_lcd1602_write_char(lcd_info, c);
-        vTaskDelay(100 / portTICK_RATE_MS);
-        ++c;
-        ++col;
-        if (col >= LCD_NUM_VISIBLE_COLUMNS)
-        {
-            ++row;
-            if (row >= LCD_NUM_ROWS)
-            {
-                row = 0;
-            }
-            col = 0;
-            i2c_lcd1602_move_cursor(lcd_info, col, row);
-        }
-    }
-
-    vTaskDelete(NULL);
-}
-
 /**
 *@brief
 *  Entry Point to Application 
@@ -752,10 +518,9 @@ void app_main(void)
     xQueueMutex = xSemaphoreCreateMutex();
     xSense_Queue = vQueueInit();
     xFlow_Queue = vQueueInit();
-    i2c_master_init();
-    printf("***I2C Master Initialized***\n");
     initialise_wifi();
     vInit_Flow();
+    setupLCD(LCD_pins);
     xTaskCreatePinnedToCore(Temp_Sense,
                             "TEMP_SENSE",
                             600,
