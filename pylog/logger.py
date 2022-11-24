d1fd0403e8b219a4aa41f21556623611407ab1ad
DmitriLyalikov <dlyalikov01@manhattan.edu>
2022-11-24 12:08:02 -0500

    Update logger.py

--- pylog/logger.py
+++ pylog/logger.py
@@ -13,6 +13,8 @@ import pandas as pd
 from pathlib import path 
 import os
 
+TEMP_TAG = "Temperature {LM35}"
+FLOW_TAG = "Flow Sensor {GR-2048}"
 class logger:
     def __init__(self, test_duration: int, output_file: str, comport: str):
         self.test_invoke_cmd = f"idf.py monitor {comport}"
@@ -39,14 +41,26 @@ class logger:
         os.chdir(self.path)
 
     def parse_monitor(self):
+        time_eq = 0
         log_file = open(self.log_file, "r")
         for line in log_file:
+            if not self.test_data["time"]:
+                # parse first time value and set to time_eq self.test_data[time][0]
+
             if TEMP_TAG in line:
+                # subtract time_eq from time and append to self.test_data
+                self.test_data 
         
             elif FLOW_TAG in line:
-                
+                # subtract time_e  from time and append to self.test_data
+
+
 
     def build_output(self):
+        # add title, info, duration
+        # construct from dict to CSV
+
+        # build plots 
 
 
 
