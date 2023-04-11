de1cffd0696fd25bdb18bdcca14f4fcc88c7811c
DmitriLyalikov <dlyalikov01@manhattan.edu>
2023-04-11 12:24:00 -0400

    added support for multiple tem sensors

--- components/LM35/LM35.c
+++ components/LM35/LM35.c
@@ -9,6 +9,8 @@ static const adc_bits_width_t width = ADC_WIDTH_12Bit;
 static const adc_atten_t atten = ADC_ATTEN_DB_11;
 static const adc_unit_t unit = ADC_UNIT_1;
 
+static esp_adc_cal_characteristics_t *adc_chars1;
+static esp_adc_cal_characteristics_t *adc_chars2;
 static const adc_channel_t ts1_channel = ADC_CHANNEL_4; // GPIO 32
 static const adc_channel_t ts2_channel = ADC_CHANNEL_5; // GPIO 33
 
@@ -27,6 +29,17 @@ void vInit_TempSense_Task(QueueHandle_t xSenseQueue, SemaphoreHandle_t xQueueMut
     adc1_config_channel_atten((adc1_channel_t)channel, atten);
     adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
     esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+
+    // Temp Sensor 1 (GPIO 32)
+    adc1_config_channel_atten((adc1_channel_t)ts1_channel, atten);
+    adc_chars1 = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+    
+    // Temp Sensor 2 (GPIO 33)
+    adc1_config_channel_atten((adc1_channel_t)ts2_channel, atten);
+    adc_chars2 = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+
     ESP_LOGI(TAG, "Starting ADC/Temp Sense Task");
     xTaskCreatePinnedToCore(vTempSense_Task,
                             "TEMP_SENSE",
@@ -51,14 +64,28 @@ void vTempSense_Task(void *pvParameters)
 {   
     for(;;){
     uint16_t temp, sum = 0;
+    uint16_t temp1, temp2 = 0;
+    uint16_t sum1 = 0;
+    uint16_t sum2 = 0;
     for (uint8_t i = 0; i < NO_OF_SAMPLES; i++){
         temp = (adc1_get_raw((adc1_channel_t)channel));    
         temp = (esp_adc_cal_raw_to_voltage(temp, adc_chars));
         sum  += temp * .1;
+        
+        temp1 = (adc1_get_raw((adc1_channel_t)ts1_channel));    
+        temp1 = (esp_adc_cal_raw_to_voltage(temp1, adc_chars1));
+        sum1  += temp1 * .1;
+
+        temp2 = (adc1_get_raw((adc1_channel_t)ts2_channel));    
+        temp2 = (esp_adc_cal_raw_to_voltage(temp2, adc_chars2));
+        sum2  += temp2 * .1;
     }
     temp = (sum / NO_OF_SAMPLES);
-    ESP_LOGI(TAG, "Temperature Read Celsius: %d", temp);
-    
+    temp1 = (sum1 / NO_OF_SAMPLES);
+    temp2 = (sum2 / NO_OF_SAMPLES);
+    ESP_LOGI(TAG, "Temperature Read on CPU,   Celsius: %d", temp);
+    ESP_LOGI(TAG, "Temperature Read Sensor 1, Celsius: %d", temp1);
+    ESP_LOGI(TAG, "Temperature Read Sensor 2, Celsius: %d", temp2);
     xSemaphoreTake(xQueueMutex, pdMS_TO_TICKS(1000));
     // Update xSense_Queue
     vUpdateQueue(xSenseQueue, TAG, temp); 
